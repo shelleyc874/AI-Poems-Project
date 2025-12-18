02e1e2427b1c9b1727edeb0076bb2c9a40bf40db
shelleyc874 <scoultish@gmail.com>
2025-12-18 14:53:39 -0500

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("Poem generated");
   new Typewriter("#poem", {
-    strings: "There once was a man from Nantucket",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: null,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+
+  let apiKey = "a40e3f3828575o8b16908tcfde6a3bd2";
+  let context =
+    "You are a funny poem expert and love to write limericks. Your mission is to generate a funny limerick in basic HTML. It does not need a title. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the limerick.";
+  let prompt = `User instructions: Generate a funny limerick about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
