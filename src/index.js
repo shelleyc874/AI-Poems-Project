b9247c82425eeede7d240ae7b0a68006f6c11853
shelleyc874 <scoultish@gmail.com>
2025-12-20 18:34:47 -0500

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -12,12 +12,15 @@ function generatePoem(event) {
   event.preventDefault();
 
   let instructionsInput = document.querySelector("#user-instructions");
-
   let apiKey = "a40e3f3828575o8b16908tcfde6a3bd2";
   let context =
-    "You are a funny poem expert and love to write limericks. Your mission is to generate a funny limerick in basic HTML. It does not need a title. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the limerick.";
+    "You are a funny poem expert and love to write limericks. Your mission is to generate a funny limerick in basic HTML. Do not write HTML at the start. It does not need a title. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the limerick.";
   let prompt = `User instructions: Generate a funny limerick about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating limerick about ${instructionsInput.value}...</div>`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
